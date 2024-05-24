0c8118f3c9708ec422623a02277d57f39448e3fd
Magnus Pettersson <magnus@entropy.nu>
2024-05-24 09:25:02 +0200

    updated backend, so it's not cloning archive anymore, it was not working good

--- containers/backend/entry.sh
+++ containers/backend/entry.sh
@@ -5,12 +5,12 @@ DIRECTORY="/app/Alpha-archives-website/backend/media/Alpha-Project-Archive"
 python /wait_for_db.py
 
 if [ ! -f /etc/configured ]; then
-    if [ -d "$DIRECTORY" ]; then
-        echo "Directory $DIRECTORY exists."
-    else
-        echo "Directory $DIRECTORY does not exist. Cloning repository..."
-        git clone https://github.com/The-Alpha-Project/Alpha-Project-Archive "$DIRECTORY"
-    fi
+    # if [ -d "$DIRECTORY" ]; then
+       # echo "Directory $DIRECTORY exists."
+    # else
+        # echo "Directory $DIRECTORY does not exist. Cloning repository..."
+        # git clone https://github.com/The-Alpha-Project/Alpha-Project-Archive "$DIRECTORY"
+    # fi
 
     cd /app/Alpha-archives-website/backend
     python manage.py makemigrations && python manage.py migrate
